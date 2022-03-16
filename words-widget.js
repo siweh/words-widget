804fc2773d0c6de3a90f5cf2000614d4ab22044f
siweh <sanemadesi@gmail.com>
2022-03-16 11:00:35 +0200

    Adding my html changes

--- words-widget.js
+++ words-widget.js
@@ -2,26 +2,36 @@ function WordsWidget() {
   function getLongestWords(sentence) {
     //console.log(sentence);
     let words = sentence.split(' ');
-    console.log(words);
+    //console.log(words);
 
     for (let i = 0; i < words.length; i++) {
       const word = words[i];
-      console.log(word);
+      //console.log(word);
       if (word.length > 4) {
         let highlitedSentence = sentence.replace(
           word,
           `<mark> ${word} </mark>`
         );
-        console.log(highlitedSentence);
+        //console.log(highlitedSentence);
         sentence = highlitedSentence;
       }
     }
     return sentence;
   }
 
+  function hideShortWords(sentence) {
+    console.log(sentence);
+    for (let i = 0; i < sentence.length; i++) {
+      const word = sentence[i];
+      
+    }
+  }
+
   function getLengthOfWords(sentence) {
+    console.log(sentence);
     let lengthOfSentence = sentence.split(' ').length;
-    return lengthOfSentence;
+    //console.log(lengthOfSentence);
+    return `There are ${lengthOfSentence} words`;
   }
 
   return {
--- words-widgets-dom.js
+++ words-widgets-dom.js
@@ -1,18 +1,27 @@
 let inputElem = document.querySelector('.addSentence');
-let displayWord = document.querySelector('.displayWords');
+let displaySentence = document.querySelector('.displayWords');
 let lengthOfWords = document.querySelector('.sentence');
+let counterBtn = document.querySelector('.counter');
 let addBtn = document.querySelector('.add-btn');
 
 const wordsList = WordsWidget();
-function clickedAddBtm() {
-  let inputValue = inputElem.value;
-  //console.log(inputValue);
-  let longestWords = wordsList.getLongestWords(inputValue);
-  //console.log(longestWords);
-  displayWord.innerHTML = longestWords;
-  let getSizeOfSentence = wordsList.getLengthOfWords(inputValue);
-  //console.log(getSizeOfSentence);
-  lengthOfWords.innerHTML = getSizeOfSentence;
+
+let inputValue;
+
+function clickedAddBtn() {
+  inputValue = inputElem.value;
+  console.log(inputValue);
+ let longestWords = wordsList.getLongestWords(inputValue);
+  console.log(longestWords);
+  displaySentence.innerHTML = longestWords;
   inputElem.value = '';
 }
-addBtn.addEventListener('click', clickedAddBtm);
+
+function clickedCounterBtn() {
+  let getSizeOfSentence = wordsList.getLengthOfWords(inputValue);
+  console.log(getSizeOfSentence);
+  lengthOfWords.innerHTML = getSizeOfSentence;
+}
+
+addBtn.addEventListener('click', clickedAddBtn);
+counterBtn.addEventListener('click', clickedCounterBtn); 
